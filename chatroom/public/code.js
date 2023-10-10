566d19cea19782a31372cec191259a05a7db18d4
mark3211996 <abacamarklouie.2020@gmail.com>
2023-10-10 16:28:07 +0800

    3rd commit

--- chatroom/public/code.js
+++ chatroom/public/code.js
@@ -1,5 +1,3 @@
-const { render } = require("vue");
-
 (function(){
 
     const app = document.querySelector(".app");
@@ -8,7 +6,6 @@ const { render } = require("vue");
     let uname;
 
     app.querySelector(".join-screen #join-user").addEventListener("click",function(){
-
         let username = app.querySelector(".join-screen #username").value;
         if(username.length == 0){
             return;
@@ -19,8 +16,7 @@ const { render } = require("vue");
         app.querySelector(".chat-screen").classList.add("active");
     });
 
-    app.querySelector(".chat-screen #send-message").addEventListener("click",function()
-    {
+    app.querySelector(".chat-screen #send-message").addEventListener("click",function(){
         let message = app.querySelector(".chat-screen #message-input").value;
         if(message.length == 0){
             return;
@@ -36,21 +32,60 @@ const { render } = require("vue");
         app.querySelector(".chat-screen #message-input").value = "";
 
     });
+    app.querySelector(".chat-screen #exit-chat").addEventListener("click",function(){
+        socket.emit("exituser",uname);
+        window.location.href = window.location.href;
+    })
+
+    socket.on("update",function(update){
+        renderMessage("update",update);
+    });
+    socket.on("chat",function(message){
+        renderMessage("other",message);
+    });
+
+
 
     function renderMessage(type,message){
         let messageContainer = app.querySelector(".chat-screen .messages");
         if(type == "my"){
             let el = document.createElement("div");
             el.setAttribute("class","message my-message");
-            el.innerHTML = '<div> <div class ="name">You</div>  <div class="text">${message.text}</div> </div>';
+            el.innerHTML = `
+            <div>
+                <div class="name">you</div>
+                <div class="text">${message.text}</div>
+            </div>
+        `;
+          
             messageContainer.appendChild(el);
 
-        } else if (type == "other")
-        {
-            
-        } else if(type == "update"){
+        } 
 
+        else if (type == "other")
+        {
+            let el = document.createElement("div");
+            el.setAttribute("class","message other-message");
+                el.innerHTML = `
+        <div>
+            <div class="name">you</div>
+            <div class="text">${message.text}</div>
+        </div>
+            `;
+          
+            messageContainer.appendChild(el);
+        } 
+        
+        else if(type == "update")
+        {
+            let el = document.createElement("div");
+            el.setAttribute("class","update");
+            el.innerText = message;
+          
+            messageContainer.appendChild(el);
         }
+        //to scroll from top message
+        messageContainer.scrollTop = messageContainer.scrollHeight - messageContainer.clientHeight;
     }
 
 })();
